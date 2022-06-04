c5f169fbabcb4d851c2359991b9e9dad640d6792
Brazil <104152835+Brazil12345@users.noreply.github.com>
2022-06-04 09:19:59 +0300

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,13 @@
+﻿
+namespace MonkeGoToBrazil
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    class PluginInfo
+    {
+        public const string GUID = "com.brazilproductions.gorillatag.monkegotobrazil";
+        public const string Name = "MonkeGoToBrazil";
+        public const string Version = "1.0.0";
+    }
+}
